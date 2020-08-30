86b022c9eefb0ed8511a8ffd33c71b001f46c4df
AnastaciaZ <zaikina.anastacia@gmail.com>
2020-08-30 16:27:05 +0300

    task1-6

---
+++ task-2.js
@@ -0,0 +1,12 @@
+const countProps = function (obj) {
+  'use strict';
+  return Object.keys(obj).length;
+};
+console.log(countProps({}));
+console.log(countProps({ a: 1, b: 1 }));
+console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 }));
+
+/*Подсчет количества свойств в объекте
+Напиши функцию countProps(obj), которая возвращает число - количество свойств в объекте.
+
+Циклы не должны использоваться*/
